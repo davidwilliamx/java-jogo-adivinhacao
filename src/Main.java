eee313804488a6091bf6cbd05a50cb39855e8da9
David Magalhães <david.williamx@gmail.com>
2024-05-08 19:08:38 -0300

    Cria código para o jogo de adivinhação.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner leitura = new Scanner(System.in);
+
+        int numeroAleatorio = new Random().nextInt(101);
+        int tentativas = 0;
+        int chuteUsuario = 0;
+
+        while (chuteUsuario != numeroAleatorio) {
+            System.out.println("Informe seu chute");
+            chuteUsuario = leitura.nextInt();
+            if (chuteUsuario == numeroAleatorio) {
+                System.out.println("Você acertou o número.");
+                break;
+            } else {
+                if (chuteUsuario > numeroAleatorio) {
+                    System.out.println("O número aleatório é MENOR.");
+                } else {
+                    System.out.println("O número aleatório é MAIOR.");
+                }
+                tentativas++;
+            }
+        }
+        String palavraTentativa = tentativas > 1 ? "vezes" : "vez";
+        System.out.println("Você acertou o número aleatório em apenas " + tentativas + " " + palavraTentativa + ".");
     }
 }
