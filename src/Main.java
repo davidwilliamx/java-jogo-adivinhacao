e9503ae36792c0fbda8ae32218653e9cce45511b
David Magalhães <david.williamx@gmail.com>
2024-05-08 19:34:27 -0300

    Corrige minha solução adiciona o número máximo de tentativas.

--- src/Main.java
+++ src/Main.java
@@ -8,23 +8,23 @@ public class Main {
         int numeroAleatorio = new Random().nextInt(101);
         int tentativas = 0;
         int chuteUsuario = 0;
+        System.out.println("Tente advinhar o numero aleatório abaixo.");
 
-        while (chuteUsuario != numeroAleatorio) {
+        while (tentativas < 5) {
             System.out.println("Informe seu chute");
             chuteUsuario = leitura.nextInt();
+            tentativas++;
             if (chuteUsuario == numeroAleatorio) {
-                System.out.println("Você acertou o número.");
+                System.out.println("Você acertou o número em " + tentativas + " de " + " 5 tentativas!");
                 break;
+            } else if (chuteUsuario > numeroAleatorio) {
+                System.out.println("O número aleatório é MENOR.");
             } else {
-                if (chuteUsuario > numeroAleatorio) {
-                    System.out.println("O número aleatório é MENOR.");
-                } else {
-                    System.out.println("O número aleatório é MAIOR.");
-                }
-                tentativas++;
+                System.out.println("O número aleatório é MAIOR.");
             }
         }
-        String palavraTentativa = tentativas > 1 ? "vezes" : "vez";
-        System.out.println("Você acertou o número aleatório em apenas " + tentativas + " " + palavraTentativa + ".");
+        if (tentativas == 5 && chuteUsuario != numeroAleatorio) {
+            System.out.println("Você não acertou o número aleatório em até 5 tentativas. O número aleatório era  " + numeroAleatorio + ".");
+        }
     }
 }
